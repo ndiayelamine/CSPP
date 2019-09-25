4dc102100eb96a06e3e1b777f05a254c0deee693
Lamine <ndiayelamine7@gmail.com>
2019-09-25 20:38:10 +0200

    comment

--- phpFunctions/scanDir.php
+++ phpFunctions/scanDir.php
@@ -14,26 +14,6 @@ $allData = array();
 $allData = dirToArray($relativePath, $pathFolder, null);
 echo json_encode(array_values($allData));
 
-
-// function scanDirectories($relativePath, $allData){
-//     $dirContent = scandir($relativePath);
-//     foreach($dirContent as $key => $content) {
-//         // filter all files not accessible
-//         $path = $relativePath.'/'.$content;
-//         if(!in_array($content, $invisibleFileNames)) {
-//             // if content is file & readable, add to array
-//             if(is_file($path) && is_readable($path)) {
-//                 // save file name with path
-//                 $allData[] = ".".$pathFolder."/".$content;
-//             // if content is a directory and readable, add path and name
-//             }elseif(is_dir($path) && is_readable($path)) {
-//                 // recursive callback to open new directory
-//                 $allData = scanDirectories($path, $allData);
-//             }
-//         }
-//     }
-// }
-
 function dirToArray($relativePath, $pathFolder, $nomeDir) {
   
     $result = array();
